17b7ed54121e5191f8f89529260c0614f93eb74f
SHANYA77 <shanyasaini2@gmail.com>
2026-04-07 21:00:27 +0530

    Update CryptoService.java

--- CryptoService.java
+++ CryptoService.java
@@ -1,12 +1,51 @@
+import java.security.MessageDigest;
+import java.security.SecureRandom;
 import java.util.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.spec.GCMParameterSpec;
 
 public class CryptoService {
 
-    public String encrypt(String data) {
-        return Base64.getEncoder().encodeToString(data.getBytes());
+    public static String hashPassword(String password) throws Exception {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] hash = md.digest(password.getBytes());
+
+        StringBuilder hex = new StringBuilder();
+        for (byte b : hash)
+            hex.append(String.format("%02x", b));
+
+        return hex.toString();
     }
 
-    public String decrypt(String encrypted) {
-        return new String(Base64.getDecoder().decode(encrypted));
+    public static String encrypt(String data) throws Exception {
+        var key = KeyService.getKey();
+
+        Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+
+        byte[] iv = new byte[12];
+        new SecureRandom().nextBytes(iv);
+
+        GCMParameterSpec spec = new GCMParameterSpec(128, iv);
+        cipher.init(Cipher.ENCRYPT_MODE, key, spec);
+
+        byte[] encrypted = cipher.doFinal(data.getBytes());
+
+        return Base64.getEncoder().encodeToString(iv) + ":" +
+               Base64.getEncoder().encodeToString(encrypted);
+    }
+
+    public static String decrypt(String encryptedData) throws Exception {
+        var key = KeyService.getKey();
+
+        String[] parts = encryptedData.split(":");
+
+        byte[] iv = Base64.getDecoder().decode(parts[0]);
+        byte[] encrypted = Base64.getDecoder().decode(parts[1]);
+
+        Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+        cipher.init(Cipher.DECRYPT_MODE, key,
+                new GCMParameterSpec(128, iv));
+
+        return new String(cipher.doFinal(encrypted));
     }
 }
